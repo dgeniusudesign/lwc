a4f7c4190d37ed2904e895568f4d0ec94a934bca
mk <dgeniusudesign@gmail.com>
2015-04-15 23:20:58 -0500

    End of Day1, Video 9

--- joins/models.py
+++ joins/models.py
@@ -3,9 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Join(models.Model):
-  email = models.EmailField()
+  email = models.EmailField(unique=True)
+  ip_address = models.CharField(max_length=120, default='ABC')
   timestamp = models.DateTimeField(auto_now_add = True, auto_now = False)
   updated = models.DateTimeField(auto_now_add = False, auto_now = True)
 
   def __unicode__(self):
     return "%s" %(self.email)
+
+
+# 1. Install south: pip install south, add south to settings.py under Installed_APPS
+# 2. Ensure Model is in sync with Database - python manage.py syncdb
+# 3. Convert the model to south : python manage.py convert_to_south appname
+# 4. Make changes to the model - eg add new fields ( ip_address = models.CharField(max_length=120, default='ABC') )
+# 5. Run schemamigration: python manage.py schemamigration app --auto
+# 6. Run migration: python manage.py migrate
--- joins/views.py
+++ joins/views.py
@@ -4,20 +4,36 @@ from django.shortcuts import render
 from .forms import EmailForm, JoinForm
 from .models import Join
 
+def get_ip(request):
+  try:
+    x_forward = request.META.get("HTTP_X_FORWARDED_FOR")
+    if x_forward:
+      ip = x_forward.split(",")[0]
+    else:
+      ip = request.META.get("REMOTE_ADDR")
+  except:
+    ip = ""
+  return ip
+
 def home(request):
-  # This is using regular Django forms
-  form = EmailForm(request.POST or None)
-  if form.is_valid():
-    email = form.cleaned_data['email']
-    new_join, created = Join.objects.get_or_create(email=email)
+  # print request.META.get("REMOTE_ADDR")
+  # print request.META.get("HTTP_X_FORWARDED_FOR")
+  # # This is using regular Django forms
+  # form = EmailForm(request.POST or None)
+  # if form.is_valid():
+  #   email = form.cleaned_data['email']
+  #   new_join, created = Join.objects.get_or_create(email=email)
 
   # # This is using ModelForm
 
-  # form = JoinForm(request.POST or None)
-  # if form.is_valid():
-  #   new_join = form.save(commit=False)
-  #   # There could be manipulations here. And hence the saving is delayed
-  #   new_join.save()
+  form = JoinForm(request.POST or None)
+  if form.is_valid():
+    new_join = form.save(commit=False)
+    # There could be manipulations here. And hence the saving is delayed
+    # email = form.cleaned_data['email']
+    # new_join_old, created = Join.objects.get_or_create(email=email)
+    new_join.ip_address = get_ip(request)
+    new_join.save()
 
   context = {"form": form}
   template = "home.html"
